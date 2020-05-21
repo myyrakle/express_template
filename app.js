469cbe097046d704d9ff0d621a83425b59c6e3ba
sanggeon <sssang97@svcorps.dev>
2020-05-22 07:17:25 +0900

    commit

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(express.static(path.join(__dirname, 'test')));
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
