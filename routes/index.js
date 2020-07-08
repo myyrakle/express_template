39e71e88d355b5205a64c8f80a42ca7de82ce1ee
myyrakle <sssang97@naver.com>
2020-07-09 07:47:18 +0900

    add views

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,11 @@ const router = express.Router();
 
 /* GET home page. */
 router.get("/test", async (req, res, next) => {
-    console.log(req.authorizer.tokenValue);
-    res.json({ msg: "돌아감" });
+    if (req.authorizer.authorized) {
+        res.json({ msg: "돌아감", value: req.authorizer.tokenValue });
+    } else {
+        res.json({ msg: "인증 안됨" });
+    }
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,8 @@ const express = require("express");
 const router = express.Router();
 
 /* GET users listing. */
-router.get("/foo", (request, response, next) => {
-  response.send("respond with a resource");
+router.get("/foo", (req, res, next) => {
+  res.send("respond with a resource");
 });
 
 module.exports = router;
